0ca2e93e113806b8a85cc4e0996a895a2128a0a7
KoShin <koshin.1116@gmail.com>
2023-07-13 08:49:46 +0800

    hello ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+// 使用 `cargo run > image.ppm` 可以生成对应的图片
 fn main() {
-    println!("Hello, world!");
+    let img_width = 256;
+    let img_height = 256;
+
+    println!("P3\n{} {}\n255\n", img_width, img_height);
+    for j in (0..img_height).rev() {
+        for i in 0..img_width {
+            let r = i as f32 / (img_width - 1) as f32;
+            let g = j as f32 / (img_height - 1) as f32;
+            let b = 0.25;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
